c46a3b91dd7abef37f070df9253eef3e327daed0
Smoren <ofigate@gmail.com>
2026-09-10 15:40:51 +0300

    upd

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def run_build_printed_mesh():
     final_mesh = concatenate_meshes(final_mesh, bottom_box_mesh)
 
     top_box_mesh = create_top_box_mesh()
-    # final_mesh = concatenate_meshes(final_mesh, top_box_mesh)
+    final_mesh = concatenate_meshes(final_mesh, top_box_mesh)
 
     print('is_watertight =', final_mesh.is_watertight)
     print('is_volume =', final_mesh.is_volume)
